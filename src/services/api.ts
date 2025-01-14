fcb90c526a1ab06efb6d4de474fa0eb7af5b790f
Matheus Rodrigues da Silveira <matheussilveira2005@gmail.com>
2025-01-14 15:11:02 -0300

    fix: ready for conection with backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333", 
+  baseURL: "https://dev-blog-backend-zeta.vercel.app", 
   headers: {
     "Content-Type": "application/json",
   },
